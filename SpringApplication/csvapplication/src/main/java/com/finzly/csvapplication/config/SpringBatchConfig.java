a6ceba02a636acaecb293c34f5fb838e2fb81c21
sharayu134 <ysharayu18@gmail.com>
2020-11-23 19:41:53 +0530

    Updated Application

--- SpringApplication/csvapplication/src/main/java/com/finzly/csvapplication/config/SpringBatchConfig.java
+++ SpringApplication/csvapplication/src/main/java/com/finzly/csvapplication/config/SpringBatchConfig.java
@@ -1,12 +1,6 @@
 package com.finzly.csvapplication.config;
 
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -14,65 +8,42 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
-import org.springframework.beans.propertyeditors.CustomNumberEditor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.data.convert.CustomConversions;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.finzly.csvapplication.model.MarginedRate;
 
+@Transactional
 @Configuration
 @EnableBatchProcessing
 public class SpringBatchConfig {
-
-    @Bean
-    public Job job(JobBuilderFactory jobBuilderFactory,
-                   StepBuilderFactory stepBuilderFactory,
-                   ItemReader<MarginedRate> itemReader,
-                   ItemProcessor<MarginedRate, MarginedRate> itemProcessor,
-                   ItemWriter<MarginedRate> itemWriter
-    ) {
-
-        Step step = stepBuilderFactory.get("ETL-file-load")
-                .<MarginedRate, MarginedRate>chunk(10000)
-                .reader(itemReader)
-                .processor(itemProcessor)
-                .writer(itemWriter)
-                .build();
-
-
-        return jobBuilderFactory.get("ETL-Load")
-                .incrementer(new RunIdIncrementer())
-                .start(step)
-                .build();
-    }
 
     @Bean
     public FlatFileItemReader<MarginedRate> itemReader() {
 
         FlatFileItemReader<MarginedRate> flatFileItemReader = new FlatFileItemReader<>();
 
-//      flatFileItemReader.setResource(new FileSystemResource("src/main/resources/ten_thousand_records.csv"));
-         flatFileItemReader.setResource(new FileSystemResource("src/main/resources/one_Lac_records.csv"));
-//           flatFileItemReader.setResource(new FileSystemResource("src/main/resources/three_lac_twenty_thousands_records.csv"));
-
-         
+//      flatFileItemReader.setResource(new FileSystemResource("src/main/resources/ten_thousand_records.csv"));//6sec
+//      flatFileItemReader.setResource(new FileSystemResource("src/main/resources/one_Lac_records.csv"));//22sec
+//      flatFileItemReader.setResource(new FileSystemResource("src/main/resources/five_Lac_records.csv"));//1minsseven_Lac_records.csv
+//      flatFileItemReader.setResource(new FileSystemResource("src/main/resources/seven_Lac_records.csv"));//1min50sec
+   
+      flatFileItemReader.setResource(new FileSystemResource("src/main/resources/ten_Lac_records.csv"));//2mins
+//spring batch with no multithreading 10L records in 5mins 
         flatFileItemReader.setName("CSV-Reader");
         flatFileItemReader.setLinesToSkip(1);//skipping header here
         flatFileItemReader.setLineMapper(lineMapper());
-        flatFileItemReader.setMaxItemCount(100000);
+        flatFileItemReader.setMaxItemCount(1000000);
         return flatFileItemReader;
     }
 
@@ -89,43 +60,51 @@ public class SpringBatchConfig {
         BeanWrapperFieldSetMapper<MarginedRate> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
         fieldSetMapper.setTargetType(MarginedRate.class);
         
-        Map< java.lang.Object, java.beans.PropertyEditor> ParseMap =new HashMap<>();
-  
-          
-        ParseMap.put(Date.class, new CustomDateEditor(new SimpleDateFormat("dd-MMM-yyyy"), false));
-        ParseMap.put(Double.class,new CustomNumberEditor( Double.class, new DecimalFormat("#####.000"),true));//Float.class, numberFormat, true
-
-        fieldSetMapper.setCustomEditors(ParseMap);
+//        Map< java.lang.Object, java.beans.PropertyEditor> ParseMap =new HashMap<>();
+//        ParseMap.put(Date.class, new CustomDateEditor(new SimpleDateFormat("dd-MMM-yyyy"), false));
+//        ParseMap.put(Double.class,new CustomNumberEditor( Double.class, new DecimalFormat("#####.000"),true));//Float.class, numberFormat, true
+//
+//        fieldSetMapper.setCustomEditors(ParseMap);
 
         defaultLineMapper.setLineTokenizer(lineTokenizer);
         defaultLineMapper.setFieldSetMapper(fieldSetMapper);
 
         return defaultLineMapper;
     }
+    
+    @Bean
+    public Job multithreadedJob(JobBuilderFactory jobBuilderFactory, ItemWriter<MarginedRate> itemWriter) throws Exception {
+        return jobBuilderFactory
+                .get("Multithreaded JOB")
+                .incrementer(new RunIdIncrementer())
+                .flow(multithreadedManagerStep(null,itemWriter))
+                .end()
+                .build();
+    }
+    @Bean
+    public Step multithreadedManagerStep(StepBuilderFactory stepBuilderFactory,ItemWriter<MarginedRate> itemWriter) throws Exception {
+        return stepBuilderFactory
+                .get("Multithreaded : Read -> Process -> Write ")
+                .<MarginedRate,  MarginedRate>chunk(10000)
+                .reader(itemReader())
+//                .processor(multithreadedchProcessor())
+                .writer(itemWriter)
+                .taskExecutor(taskExecutor())
+                .build();
+    }
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(500);
+//        executor.setMaxPoolSize(500);
+//        executor.setQueueCapacity(500);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setThreadNamePrefix("MultiThreaded-");
+        return executor;
+    }
+
 
 }
-//  class CS implements ConversionService {
-//
-//    @Override
-//    public boolean canConvert(Class<?> sourceType, Class<?> targetType) {
-//        return sourceType == String.class && targetType == double.class;
-//    }
-//
-//    @Override
-//    public boolean canConvert(TypeDescriptor sourceType, TypeDescriptor targetType) {
-//        return sourceType.equals(TypeDescriptor.valueOf(String.class)) &&
-//               targetType.equals(TypeDescriptor.valueOf(double.class)) ;
-//    }
-//
-//    @Override
-//    public <T> T convert(Object source, Class<T> targetType) {
-//        return (T)Double.valueOf(source.toString().replace(',', '.'));
-//    }
-//
-//    @Override
-//    public Object convert(Object source, TypeDescriptor sourceType, TypeDescriptor targetType) {
-//        return Double.valueOf(source.toString().replace(',', '.'));
-//    }
-//}
+
 
 //https://www.youtube.com/watch?v=1XEX-u12i0A
